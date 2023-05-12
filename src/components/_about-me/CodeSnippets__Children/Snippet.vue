3323c6377377ab191deb526b18ca53fee459d6dd
Adan <999a.d.a.n.a.y.a.z999@gmail.com>
2023-05-12 11:13:06 +0500

    External code highlighting css file

--- src/components/_about-me/CodeSnippets__Children/Snippet.vue
+++ src/components/_about-me/CodeSnippets__Children/Snippet.vue
@@ -8,6 +8,7 @@ import "vue-prism-editor/dist/prismeditor.min.css";
 import { highlight, languages } from "prismjs/components/prism-core";
 import "prismjs/components/prism-clike";
 import "prismjs/components/prism-javascript";
+import "../../prismtheme/prism-coldark-dark.css";
 
 const { index, codeText, details, scrollOnOpen } = defineProps([
   "index",
