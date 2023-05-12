b95f815ed31860a1578ab478a8c6e7394bcae104
Adan <999a.d.a.n.a.y.a.z999@gmail.com>
2023-05-12 11:14:36 +0500

    Imports fix

--- src/components/_contact-me/ContactSection.vue
+++ src/components/_contact-me/ContactSection.vue
@@ -6,7 +6,7 @@ import "vue-prism-editor/dist/prismeditor.min.css";
 import { highlight, languages } from "prismjs/components/prism-core";
 import "prismjs/components/prism-clike";
 import "prismjs/components/prism-javascript";
-import "../../prismtheme/prism-coldark-dark.css";
+import "../prismtheme/prism-coldark-dark.css";
 
 const name = ref("");
 const email = ref("");
